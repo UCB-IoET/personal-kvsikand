3ff01026fc62de9ee1b9d7bb461e12a9813222a6
mschuldt <mbschuldt@gmail.com>
2015-02-15 19:08:00 -0800

    basic heater functions and services

--- week3/heaterNode.lua
+++ week3/heaterNode.lua
@@ -9,6 +9,10 @@ ipaddr = storm.os.getipaddr()
 
 node = Node:new("Heater interfaced device")
 
+local heaterPin = storm.io.D4
+local heaterIsOn = 0
+storm.io.set(storm.io.LOW, heaterPin)
+
 getAverageTemperature = function()
 
 	local neighbors = node:getNeighborsForService("getTemperature")
@@ -38,6 +42,31 @@ getAverageTemperature = function()
 	end
 	return sum/n
 end
+
+function heaterOn()
+   storm.io.set(storm.io.HIGH, heaterPin)
+   heaterIsOn = 1;
+end
+
+function heaterOff()
+   storm.io.set(storm.io.LOW, heaterPin)
+   heaterIsOn = 0;
+end
+
+function setHeater(state)
+   if state == 1 then
+      heaterOn()
+   else
+      heaterOff()
+   end
+end
+
+function setTargetTemp(temp)
+   return nil --TODO
+end
+
+node:addService("setHeater","setBool","turn heater on/off", setHeater)
+node:addService("setTemp","setNumber","set target temperature", setTargetTemp)
 
 -- enable a shell
 sh = require "stormsh"
