76eea757a21397a36ad4bf03e63340a97cde7422
mschuldt <mbschuldt@gmail.com>
2015-03-12 11:42:14 -0700

    heater control

--- deviceProject/thermometer.lua
+++ deviceProject/thermometer.lua
@@ -1,13 +1,32 @@
 require "cord"
 sh = require("stormsh")
 require"svcd"
-storm.n.svcd_init("thermometer", function()
-	SVCD.add_service("RoomTemperature")
-	SVCD.add_attribute(0x3005, 0x3540, function() 
-		print("set the room temperature")
-	end)
-	
-end)
+local room_temp_id = 0x3055
+SVCD.init("heater_control",
+	  function()
+	     --print("SVCD_INIT()")
+	     SVCD.add_service(room_temp_id) --"RoomTemperature"
+	     SVCD.add_attribute(room_temp_id,
+				0x3540,
+				function (payload, port, ip)
+				   print("set the room temperature")
+				end)
+	  end)
+
+-- true/false
+function use_contact_sensor(on)
+   SVCD.write("fe80::212:6d02:0:3039", 0x162e, 0x03e9, on, 500, function ()print("ok.") end)
+end
+
+-- 1/0
+function set_heater_on(on)
+   SVCD.write("fe80::212:6d02:0:3039", 0x0d80, 0x0001, on , 500, function ()print("ok.") end)
+end
+
+function set_target_temp(temp)
+   SVCD.write("fe80::212:6d02:0:3039", 0x11d7, 0x03e9, temp , 500, function ()print("ok.") end)
+end
+
 
 sh.start()
 cord.enter_loop()
