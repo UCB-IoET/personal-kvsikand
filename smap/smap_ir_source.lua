546120caeadd9b8be6242eefa9e586a1c9bcbc27
mschuldt <mbschuldt@gmail.com>
2015-04-03 16:30:11 -0700

    smap ir temp source

---
+++ smap/smap_ir_source.lua
@@ -0,0 +1,31 @@
+require "cord"
+
+TEMP = require "temp"
+temp = TEMP:new()
+
+middleware_ip = "2001:470:66:b0::2"
+udp_port = "1263"
+
+sendsock = storm.net.udpsocket(1337, function() end)
+
+cord.new(function ()
+	    temp:init()
+	    ir = temp:getIRTemp();
+	    iter=0
+	    while true do
+	       iter = iter + 1;
+	       ir_new = temp:getIRTemp()
+	       ir = (ir_new*7000 + ir*3000)/10000	       
+	       if iter % 5 == 0 then
+		  iter = 0
+		  amb = temp:getTemp() 
+		  t = {amb, ir}
+		  storm.net.sendto(sendsock, storm.mp.pack(t), middleware_ip, udp_port)
+		  print("amb = " .. amb
+			   .. "   IR =  "..ir.."C, "..storm.n.c_to_f(ir).."F")
+	       end
+	       cord.await(storm.os.invokeLater, 200 * storm.os.MILLISECOND)
+	    end
+	 end)
+
+cord.enter_loop()
